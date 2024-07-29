2c73efc11d76c93c6ead2e4628938062e6fc1b7a
mudimurtala <ibnmuhyideen95@gmail.com>
2024-07-29 15:38:31 +0100

    Add Math functions and operation variables

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Error: Divition by zero"
+    }
+    return a / b;
+}
